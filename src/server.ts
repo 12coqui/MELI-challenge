3987997f40ee49b18ccddf5512e7c209f15a8904
12coqui <coquiserra1992@gmail.com>
2024-11-09 11:05:40 -0300

    fixing deploy

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,19 @@ const routes = require('./Routes/MutantRoutes');
 const app = express();
 
 app.use(express.json());
-app.use('/', routes);
-
+app.use('/api', routes);
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 moongoose
-	.connect(process.env.MONGODB_URI || '')
-	.then(() => {
-		const PORT = process.env.PORT || 3000;
-		app.listen(PORT, () => {
-			console.log(`Server is running on port ${PORT}`);
-		});
-		console.log('Connected to MongoDB');
-	})
-	.catch((err: any) => {
-		console.log(err);
-	});
+  .connect(process.env.MONGODB_URI || '')
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+    console.log('Connected to MongoDB');
+  })
+  .catch((err: any) => {
+    console.log(err);
+  });
